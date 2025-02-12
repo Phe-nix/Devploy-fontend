c6bc87ff9fea23b65eb8375d6fe6779b9f22c9bb
Phe-nix <spicedexdee123@gmail.com>
2025-02-13 04:42:09 +0700

    ✅ fetch data

---
+++ src/routes/(auth)/[workspaceSlug]/+layout.server.ts
@@ -0,0 +1,14 @@
+import type { LayoutServerLoad } from './$types';
+
+export const load: LayoutServerLoad = async ({ parent, params, fetch }) => {
+	const { accessToken } = await parent();
+	const { workspaceSlug } = params;
+	const workspaceInfo = await fetch(`http://127.0.0.1:3000/workspace/${workspaceSlug}`, {
+		headers: {
+			Authorization: `Bearer ${accessToken}`
+		}
+	});
+	return {
+        info: workspaceInfo.ok ? await workspaceInfo.json() : null
+    };
+};
