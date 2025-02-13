eb56bfad2510858f7c14ef12089f869242b0cd5e
Phe-nix <spicedexdee123@gmail.com>
2025-02-14 04:19:52 +0700

    🔧 fix route

--- src/routes/(auth)/+page.ts
+++ src/routes/(auth)/+page.ts
@@ -4,7 +4,7 @@ import type { PageLoad } from './$types';
 export const load: PageLoad = async ({ parent }) => {
 	const { workspace } = await parent();
     if (workspace.length !== 0) {
-        redirect(302, '/' + workspace[0].slug);
+        redirect(302, '/' + workspace[0].slug + "/applications");
     }
 	return {};
 };
