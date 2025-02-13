c672f948e721572d59d464f09d90ec7c75c52e3b
Phe-nix <spicedexdee123@gmail.com>
2025-02-14 04:20:36 +0700

    🏗️ make new achitech

--- src/routes/(auth)/[workspaceSlug]/+layout.svelte
+++ src/routes/(auth)/[workspaceSlug]/+layout.svelte
@@ -1,18 +1,44 @@
 <script lang="ts">
-    // import components
-    import Navbar from "$lib/components/customs/navbar/navbar.svelte";
-    
-    // import data
-    import type { LayoutProps } from "./$types";
-    import { setContext } from 'svelte';
-    
-    let { data, children }: LayoutProps = $props();
+	// shadcn-svelte
+	import * as Tabs from '$lib/components/ui/tabs';
+	import { Button } from '$lib/components/ui/button/index.js';
 
-    // console.log(data)
+	// import components
+	import Navbar from '$lib/components/customs/navbar/navbar.svelte';
+
+	// import data
+	import type { LayoutProps } from './$types';
+	import { goto } from '$app/navigation';
+    import { page } from '$app/stores';
+
+	let { data, children }: LayoutProps = $props();
 </script>
 
-<Navbar workspaces={data}/>
-
+<Navbar workspaces={data} />
 <div class="max-w-sm w-full md:max-w-2xl lg:max-w-4xl xl:max-w-6xl mx-auto min-h-full">
-    {@render children()}
+	<div>
+		<div class="flex justify-between items-center">
+			<h2
+				class="scroll-m-20 pb-2 text-2xl sm:text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+			>
+				{$page.params.workspaceSlug}
+			</h2>
+		</div>
+		<Tabs.Root value="app" class="w-full pt-6">
+			<Tabs.List class="grid w-full grid-cols-3">
+				<Tabs.Trigger value="app" onclick={() => { goto(`/${$page.params.workspaceSlug}/applications`)}}>Applications</Tabs.Trigger>
+				<Tabs.Trigger value="database" onclick={() => { goto(`/${$page.params.workspaceSlug}/databases`)}}>Databases</Tabs.Trigger>
+				<Tabs.Trigger value="setting" onclick={() => { goto(`/${$page.params.workspaceSlug}/settings`)}}>Setting</Tabs.Trigger>
+			</Tabs.List>
+			<Tabs.Content value="app">
+				{@render children()}
+			</Tabs.Content>
+			<Tabs.Content value="database">
+				{@render children()}
+			</Tabs.Content>
+			<Tabs.Content value="setting">
+				{@render children()}
+			</Tabs.Content>
+		</Tabs.Root>
+	</div>
 </div>
