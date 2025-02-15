bb0daf07e0c73e2aa9395492d46d452ebd43c5ef
Phe-nix <spicedexdee123@gmail.com>
2025-02-15 16:17:46 +0700

    💄 update profile avatar and label to use userProfile data

--- src/lib/components/customs/navbar/setting-profile.svelte
+++ src/lib/components/customs/navbar/setting-profile.svelte
@@ -8,19 +8,22 @@
     import { LogOut } from "lucide-svelte";
     import { UserRound } from "lucide-svelte";
     import { Settings } from 'lucide-svelte';
+
+    const { userProfile } = $props();
+    
   </script>
   
   <DropdownMenu.Root>
     <DropdownMenu.Trigger>
       <Button variant="ghost">
         <Avatar.Root>
-          <Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+          <Avatar.Image src={userProfile.picture} alt="@shadcn" />
           <Avatar.Fallback>CN</Avatar.Fallback>
         </Avatar.Root>
       </Button>
     </DropdownMenu.Trigger>
     <DropdownMenu.Content align="start" class="w-56 ">
-      <DropdownMenu.Label class="text-lg">My profile</DropdownMenu.Label>
+      <DropdownMenu.Label class="text-lg">{userProfile.firstName} {userProfile.lastName}</DropdownMenu.Label>
       <DropdownMenu.Separator />
       <DropdownMenu.Item class="flex flex-row gap-x-2 cursor-pointer">
           <Settings class="text-primary"/>
