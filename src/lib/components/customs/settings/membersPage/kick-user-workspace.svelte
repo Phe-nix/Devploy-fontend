33f4ca3cefec97ea1da161a3b1c2be5f91e50679
Phe-nix <spicedexdee123@gmail.com>
2025-02-17 21:17:58 +0700

    💄 add kick user functionality with alert dialog for workspace management

---
+++ src/lib/components/customs/settings/membersPage/kick-user-workspace.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { PUBLIC_BASE_API } from '$env/static/public';
+	import * as AlertDialog from '$lib/components/ui/alert-dialog';
+	import { Button } from '$lib/components/ui/button/index.js';
+	import { cn } from '$lib/utils';
+	import axios from 'axios';
+	import { toast } from 'svelte-sonner';
+
+	// let { permisstion, slug } = $props();
+	let { class: className } = $props();
+
+	let isOpen = $state(false);
+
+	// const delteWorkspace = async () => {
+	// 	const res = axios.delete(`${PUBLIC_BASE_API}/workspace/${slug}`, {
+	// 		headers: {
+	// 			'Content-Type': 'application/json',
+	// 			authorization: `Bearer ${permisstion.accessToken}`
+	// 		}
+	// 	});
+	// 	toast.promise(res, {
+	// 		loading: 'Loading...',
+	// 		success: (data) => {
+	// 			goto(`/${$page.params.workspaceSlug}/settings/workspaces`, {
+	// 				invalidateAll: true
+	// 			});
+	// 			return slug + ' has been deleted';
+	// 		},
+	// 		error: (e: any) => {
+	// 			return e.message;
+	// 		}
+	// 	});
+	// };
+</script>
+
+<div class={cn("",className)}>
+	<AlertDialog.Root
+		open={isOpen}
+		onOpenChange={(open) => {
+			isOpen = open;
+		}}
+	>
+		<AlertDialog.Trigger>
+			<Button variant="destructive" size="sm">
+				<p>Kick</p>
+			</Button>
+		</AlertDialog.Trigger>
+		<AlertDialog.Content>
+			<AlertDialog.Header>
+				<AlertDialog.Title>Are you absolutely sure to delete?</AlertDialog.Title>
+				<AlertDialog.Description>
+					This action cannot be undone. This will permanently delete your service.
+				</AlertDialog.Description>
+			</AlertDialog.Header>
+			<AlertDialog.Footer>
+				<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+				<AlertDialog.Action
+					onclick={() => {
+						isOpen = false;
+						// delteWorkspace();
+					}}>Continue</AlertDialog.Action
+				>
+			</AlertDialog.Footer>
+		</AlertDialog.Content>
+	</AlertDialog.Root>
+</div>
