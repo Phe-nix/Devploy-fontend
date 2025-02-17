6aeeca71210df3d6194e203c1d4d99a3b55c528d
Phe-nix <spicedexdee123@gmail.com>
2025-02-17 21:18:20 +0700

    💄 add edit user workspace functionality with name update and toast notifications

--- src/lib/components/customs/settings/workspacesPage/dialog/edit-user-workspace.svelte
+++ src/lib/components/customs/settings/workspacesPage/dialog/edit-user-workspace.svelte
@@ -5,11 +5,50 @@
 	import { Label } from '$lib/components/ui/label/index.js';
 	import { toast } from 'svelte-sonner';
 	import { PackageSearch } from 'lucide-svelte';
+	import axios from 'axios';
+	import { PUBLIC_BASE_API } from '$env/static/public';
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+
+	let { data, workspace } = $props();
+	let isOpen = $state(false);
+	let name = $state(data.name);
+	const changeNameWorkspace = async () => {
+		const res = axios.put(
+			`${PUBLIC_BASE_API}/workspace/${data.slug}`,
+			{
+				name: name
+			},
+			{
+				headers: {
+					'Content-Type': 'application/json',
+					authorization: `Bearer ${workspace.accessToken}`
+				}
+			}
+		);
+		toast.promise(res, {
+			loading: 'Loading...',
+			success: (data) => {
+				goto(`/${$page.params.workspaceSlug}/settings/workspaces`, {
+					invalidateAll: true
+				});
+				return name + ' has been updated';
+			},
+			error: (e: any) => {
+				return e.message;
+			}
+		});
+	};
 </script>
 
-<Dialog.Root>
+<Dialog.Root
+	open={isOpen}
+	onOpenChange={(open) => {
+		isOpen = open;
+	}}
+>
 	<Dialog.Trigger>
-		<Button size="sm" class="hover:text-[hsl(192,91%,36%)]">
+		<Button size="sm" class="">
 			<p>Edit</p>
 		</Button>
 	</Dialog.Trigger>
@@ -20,19 +59,21 @@
 				Make changes to your service here. Click save when you're done.
 			</Dialog.Description>
 		</Dialog.Header>
-		<div class="grid gap-4 py-4">
-			<div class="grid grid-cols-4 items-center gap-4">
-				<Label for="name" class="text-right">Name</Label>
-				<Input id="name" value="Zela-Frontend" class="col-span-3" />
+		<form
+			onsubmit={() => {
+				changeNameWorkspace();
+				isOpen = false
+			}}
+		>
+			<div class="grid gap-4 py-4">
+				<div class="grid grid-cols-4 items-center gap-4">
+					<Label for="name" class="text-right">Name</Label>
+					<Input id="name" bind:value={name} class="col-span-3" />
+				</div>
 			</div>
-		</div>
-		<Dialog.Footer>
-			<Button
-				type="submit"
-				onclick={() => {
-					toast.success('Edit success.');
-				}}>Save changes</Button
-			>
-		</Dialog.Footer>
+			<Dialog.Footer>
+				<Button type="submit">Save changes</Button>
+			</Dialog.Footer>
+		</form>
 	</Dialog.Content>
 </Dialog.Root>
