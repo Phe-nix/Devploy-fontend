0ae3db28c7ba740dbfce7986aa9ede7e6a105bc7
Phe-nix <spicedexdee123@gmail.com>
2025-02-21 21:06:18 +0700

    💄 add initial structure for workspace page server actions

---
+++ src/routes/(auth)/[workspaceSlug]/+page.server.ts
@@ -0,0 +1,13 @@
+// import type { Actions } from './$types';
+
+// export const actions = {
+//     logout: async ({ cookies }) => {
+//         const accesss = cookies.get('accessToken')
+//         const refresh = cookies.get('refreshToken')
+
+
+//         return { Token: accesss,
+//                 ref: refresh
+//          }
+//     }
+// } satisfies Actions;
