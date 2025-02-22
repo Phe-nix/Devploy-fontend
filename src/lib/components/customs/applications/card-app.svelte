0c6a3a141a82bd11796f9408057042338fb947ab
Phe-nix <spicedexdee123@gmail.com>
2025-02-22 21:44:38 +0700

    💄 add status and update info app

--- src/lib/components/customs/applications/card-app.svelte
+++ src/lib/components/customs/applications/card-app.svelte
@@ -10,50 +10,58 @@
 	import { Ellipsis } from 'lucide-svelte';
 	import { Github } from 'lucide-svelte';
 	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
 
 	// compo
 	// import EditServices from "./applications/edit-services.svelte";
 	// import AlertDelete from "./applications/alert-delete.svelte";
 
-	let service = $props();
+	let generateImg = (url: string) => {
+		const match = url.match(/^https:\/\/github\.com\/([^\/]+)\//);
+		return match ? match[1] : null;
+	};
+
+	let { service } = $props();
 </script>
 
 <div class="relative">
-	<Popover.Root>
-		<Popover.Trigger class="absolute z-50 top-0 right-0 p-2 hover:bg-zinc-900 rounded-lg">
-			<Ellipsis />
-		</Popover.Trigger>
-		<Popover.Content class="w-28">
-			<div class="flex flex-col items-center text-center z-40">
-				<!-- <EditServices />
-          <AlertDelete/> -->
-			</div>
-		</Popover.Content>
-	</Popover.Root>
-	<button class="z-0" onclick={() => {}}>
+	<button
+		class="z-0"
+		onclick={() => {
+			goto(`/${$page.params.workspaceSlug}/application/${service.id}`);
+		}}
+	>
 		<div
-			class="transition duration-300 hover:ring-zinc-600 hover:ring-1 p-6 max-w-xs w-80 border bg-card text-card-foreground shadow-sm rounded-lg relative"
+			class="transition duration-300 hover:shadow-xl hover:ring-1 p-6 max-w-xs w-80 border bg-card text-card-foreground shadow-sm rounded-lg"
 		>
-			<div class="flex space-x-5 items-center">
+			<div class="flex space-x-5 items-center z-10">
 				<Avatar.Root>
-					<Avatar.Image src={service.img} />
+					<Avatar.Image src={`https://github.com/${generateImg(service.gitHub)}.png`} />
 					<Avatar.Fallback>CN</Avatar.Fallback>
 				</Avatar.Root>
 				<div class="flex flex-col items-start space-y-2">
-					<h1 class="text-xl font-bold">{service.name}</h1>
+					<div class="flex items-center gap-x-2">
+						<h1 class="text-xl font-bold">{service.name}</h1>
+						{#if service.status == 'inProgress'}
+							<div class="p-1 rounded bg-orange-500"></div>
+						{:else if service.status == 'Deployed'}
+							<div class="p-1 rounded bg-green-500"></div>
+						{:else if service.status == 'Failed'}
+							<div class="p-1 rounded bg-red-500"></div>
+						{:else}
+							<div class="p-1 rounded bg-gray-500"></div>
+						{/if}
+					</div>
 					<p class="text-sm text-muted-foreground">
 						Create at {service.time} minutes ago
 					</p>
 				</div>
 			</div>
 			<div class="mt-4 text-left">
-				<a
-					href={'https://github.com/' + service.linkGit}
-					class={badgeVariants({ variant: 'default' })}
-				>
+				<a href={service.gitHub} class={badgeVariants({ variant: 'default' })}>
 					<div class="p-1 flex items-center space-x-2">
 						<Github class="size-5" />
-						<p>{service.linkGit}</p>
+						<p>{service.name}</p>
 					</div>
 				</a>
 			</div>
