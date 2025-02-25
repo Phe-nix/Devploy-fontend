1c5fc8f2fec0771ec836b11e81a10c1d64dfe694
Phe-nix <spicedexdee123@gmail.com>
2025-02-25 21:29:43 +0700

    💄 add validation schema for confirm delete application form

---
+++ src/lib/types/validate/application/confirm-delete-schema.ts
@@ -0,0 +1,22 @@
+import { z } from 'zod';
+
+export const confirmDeleteForm = z
+	.object({
+		name: z.string().regex(/^devploy\/(.+)$/, { message: "The apllication name is required" }), // ต้องมีชื่อโปรเจคต่อท้าย
+		confirm: z.string()
+	})
+	.refine(
+		(data) => {
+			const projectNameMatch = data.name.match(/^devploy\/(.+)$/);
+			if (!projectNameMatch) return false; // กันกรณีผิดพลาด
+
+			const projectName = projectNameMatch[1]; // ดึง "ชื่อโปรเจค"
+			return data.confirm === `delete ${projectName}`; // เช็คว่า confirm ตรงกับ "delete ชื่อโปรเจค"
+		},
+		{
+			message: "The confirm text is required",
+			path: ['confirm']
+		}
+	);
+
+export type ConfirmDeleteForm = typeof confirmDeleteForm;
