9b4f762741c1893c23abc30f0d635cbfa8ea6233
Phe-nix <spicedexdee123@gmail.com>
2025-03-04 22:20:29 +0700

    💄 update scrolling log

--- src/routes/(auth)/[workspaceSlug]/application/[appId]/logs/+page.svelte
+++ src/routes/(auth)/[workspaceSlug]/application/[appId]/logs/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import type { PageData } from '../info/$types';
+	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
 
 	type Props = {
 		data: PageData;
@@ -10,10 +11,12 @@
 
 <div class="my-4 flex flex-col space-y-5">
 	<h3 class="scroll-m-20 text-2xl font-semibold tracking-tight">Application Build Logs</h3>
-	<div class="py-10 px-4 rounded border shadow w-full dark:bg-zinc-900 bg-zinc-100 text-zinc-100">
-        <p>Application logs....</p>
-		{#each data.appInfo.logs as log}
-			<p class="hover:bg-slate-100">{log}</p>
-		{/each}
-	</div>
+	<ScrollArea class="h-96">
+		<div class="py-5 px-4 rounded border shadow w-full dark:bg-zinc-900 bg-zinc-100 dark:text-zinc-100 text-zinc-900">
+			<p>Application logs....</p>
+			{#each data.appInfo.logs as log}
+				<p class="dark:hover:bg-slate-500 hover:bg-slate-300">{log}</p>
+			{/each}
+		</div>
+	</ScrollArea>
 </div>
