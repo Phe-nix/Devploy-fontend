974e4ead32762cb221b4f0e039a054b5fc2eb5e4
Phe-nix <spicedexdee123@gmail.com>
2025-03-20 03:42:23 +0700

    💄 update delete workspace

--- src/lib/components/customs/settings/workspacesPage/dialog/delete-user-workspace.svelte
+++ src/lib/components/customs/settings/workspacesPage/dialog/delete-user-workspace.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
+	import { goto, invalidateAll } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { PUBLIC_BASE_API } from '$env/static/public';
 	import * as AlertDialog from '$lib/components/ui/alert-dialog';
@@ -7,7 +7,7 @@
 	import axios from 'axios';
 	import { toast } from 'svelte-sonner';
 
-	let { permisstion, slug } = $props();
+	let { permisstion, slug, role } = $props();
 	let isOpen = $state(false);
 
 	const delteWorkspace = async () => {
@@ -30,34 +30,87 @@
 			}
 		});
 	};
+
+	const leaveWorkspace = async () => {
+		const req = axios.delete(`${PUBLIC_BASE_API}/workspace/${slug}/member`, {
+			data: {
+				email: permisstion.userProfile.email
+			},
+			headers: {
+				authorization: `Bearer ${permisstion.accessToken}`
+			}
+		});
+		toast.promise(req, {
+			loading: 'Loading...',
+			success: (data) => {
+				invalidateAll();
+				return 'You have been leaved workspace';
+			},
+			error: (e: any) => {
+				return e.message;
+			}
+		});
+	};
 </script>
 
-<AlertDialog.Root
-	open={isOpen}
-	onOpenChange={(open) => {
-		isOpen = open;
-	}}
->
-	<AlertDialog.Trigger>
-		<Button variant="destructive" size="sm">
-			<p>Leave</p>
-		</Button>
-	</AlertDialog.Trigger>
-	<AlertDialog.Content>
-		<AlertDialog.Header>
-			<AlertDialog.Title>Are you absolutely sure to delete?</AlertDialog.Title>
-			<AlertDialog.Description>
-				This action cannot be undone. This will permanently delete your service.
-			</AlertDialog.Description>
-		</AlertDialog.Header>
-		<AlertDialog.Footer>
-			<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
-			<AlertDialog.Action
-				onclick={() => {
-					isOpen = false;
-					delteWorkspace();
-				}}>Continue</AlertDialog.Action
-			>
-		</AlertDialog.Footer>
-	</AlertDialog.Content>
-</AlertDialog.Root>
+{#if role == 'OWNER'}
+	<AlertDialog.Root
+		open={isOpen}
+		onOpenChange={(open) => {
+			isOpen = open;
+		}}
+	>
+		<AlertDialog.Trigger>
+			<Button variant="destructive" size="sm">
+				<p>Delete</p>
+			</Button>
+		</AlertDialog.Trigger>
+		<AlertDialog.Content>
+			<AlertDialog.Header>
+				<AlertDialog.Title>Are you absolutely sure to delete?</AlertDialog.Title>
+				<AlertDialog.Description>
+					This action cannot be undone. This will permanently delete your workspace.
+				</AlertDialog.Description>
+			</AlertDialog.Header>
+			<AlertDialog.Footer>
+				<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+				<AlertDialog.Action
+					onclick={() => {
+						isOpen = false;
+						delteWorkspace();
+					}}>Continue</AlertDialog.Action
+				>
+			</AlertDialog.Footer>
+		</AlertDialog.Content>
+	</AlertDialog.Root>
+{:else if role == 'MEMBER'}
+	<AlertDialog.Root
+		open={isOpen}
+		onOpenChange={(open) => {
+			isOpen = open;
+		}}
+	>
+		<AlertDialog.Trigger>
+			<Button variant="destructive" size="sm">
+				<p>Leave</p>
+			</Button>
+		</AlertDialog.Trigger>
+		<AlertDialog.Content>
+			<AlertDialog.Header>
+				<AlertDialog.Title>Are you absolutely sure to leave?</AlertDialog.Title>
+				<AlertDialog.Description>
+					This action cannot be undone. This will leave workspace.
+				</AlertDialog.Description>
+			</AlertDialog.Header>
+			<AlertDialog.Footer>
+				<AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
+				<AlertDialog.Action
+					onclick={() => {
+						isOpen = false;
+						leaveWorkspace();
+					}}>Continue</AlertDialog.Action
+				>
+			</AlertDialog.Footer>
+		</AlertDialog.Content>
+	</AlertDialog.Root>
+{/if}
