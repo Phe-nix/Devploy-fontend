4db3a206465d816072a11f80adf86dd16e2d64a4
Phe-nix <spicedexdee123@gmail.com>
2025-03-27 01:21:56 +0700

    ⚡️ Fix form

--- src/routes/(auth)/admin/[settingSlug]/+page.server.ts
+++ src/routes/(auth)/admin/[settingSlug]/+page.server.ts
@@ -2,9 +2,6 @@ import { PUBLIC_BASE_API } from '$env/static/public';
 import { fail } from '@sveltejs/kit';
 import type { PageServerLoad, Actions } from './$types';
 
-import { superValidate } from 'sveltekit-superforms';
-import { formSchemalimit } from '$lib/types/validate/databases/setting/form-limit';
-import { zod } from 'sveltekit-superforms/adapters';
 
 export const load: PageServerLoad = async ({ parent, params, fetch }) => {
 	const { accessToken } = await parent();
@@ -15,20 +12,6 @@ export const load: PageServerLoad = async ({ parent, params, fetch }) => {
 	});
 	return {
 		alluser: allUsers.ok ? await allUsers.json() : null,
-		form: await superValidate(zod(formSchemalimit))
 	};
 };
 
-export const actions: Actions = {
-	default: async (event) => {
-		const form = await superValidate(event, zod(formSchemalimit));
-		if (!form.valid) {
-			return fail(400, {
-				form
-			});
-		}
-		return {
-			form
-		};
-	}
-};
