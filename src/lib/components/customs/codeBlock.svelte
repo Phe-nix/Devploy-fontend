707caca35e4016416e52863ec00cf3c163ea1fed
Phe-nix <spicedexdee123@gmail.com>
2025-03-28 22:44:01 +0700

    ✨ : Add custom code block component with syntax highlighting

---
+++ src/lib/components/customs/codeBlock.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import Prism from 'prismjs';
+
+	let { code, language, class: className } = $props();
+</script>
+
+<div class={cn('rounded-xl p-4 border-card bg-zinc-900 text-white', className)}>
+	{@html Prism.highlight(code, Prism.languages[language], language)}
+</div>
